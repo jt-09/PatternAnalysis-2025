19c80a8667a73cd8ded9a2277675f60b3c25c422
Jay Thakkar <jay.thakkar0514@gmail.com>
2025-10-27 19:00:42 +1000

    feat(train): Added param based epochs functionality

--- recognition/Siamese_network_s4786177/train.py
+++ recognition/Siamese_network_s4786177/train.py
@@ -8,6 +8,8 @@ Stage: Basically barebones
 
 from __future__ import annotations
 from typing import Tuple
+import numpy as np
+import matplotlib.pyplot as plt
 
 import torch
 import torch.nn as nn
@@ -19,6 +21,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 def train_one_epoch(
+    epochs: int = 3,
     batch_size: int = 32,
     lr: float = 1e-4,
     emb_dim: int = 128,
@@ -31,38 +34,44 @@ def train_one_epoch(
     model = SiameseNet(emb_dim=emb_dim, drop=drop, pretrained=True).to(device)
     opt = torch.optim.Adam(model.parameters(), lr=lr)
     ce  = nn.CrossEntropyLoss()
+    
+    history = {"train_acc": [], "val_acc": []} # track values
 
-    # ---------------- train (1 epoch) ----------------
-    model.train()
-    train_correct, train_count = 0, 0
-    for x, y, _ in train_loader:
-        x = x.to(device, non_blocking=True)
-        y = y.to(device, non_blocking=True)
-
-        _, logits = model(x)
-        loss = ce(logits, y)
-
-        opt.zero_grad(set_to_none=True)
-        loss.backward()
-        opt.step()
-
-        train_correct += (logits.argmax(1) == y).sum().item()
-        train_count   += x.size(0)
-
-    # ---------------- validate ----------------
-    model.eval()
-    val_correct, val_count = 0, 0
-    with torch.no_grad():
-        for x, y, _ in val_loader:
+    # ---------------- train (based on epoch param) ----------------
+    for epoch in range(1, epochs + 1):
+        model.train()
+        train_correct, train_count = 0, 0
+        for x, y, _ in train_loader:
             x = x.to(device, non_blocking=True)
             y = y.to(device, non_blocking=True)
-            _, logits = model(x)
-            val_correct += (logits.argmax(1) == y).sum().item()
-            val_count   += x.size(0)
 
-    train_acc = train_correct / max(1, train_count)
-    val_acc   = val_correct   / max(1, val_count)
-    print(f"train accuracy: {train_acc:.3f} | val accuracy: {val_acc:.3f}")
+            _, logits = model(x)
+            loss = ce(logits, y)
+
+            opt.zero_grad(set_to_none=True)
+            loss.backward()
+            opt.step()
+
+            train_correct += (logits.argmax(1) == y).sum().item()
+            train_count   += x.size(0)
+
+        # ------ validate ------
+        model.eval()
+        val_correct, val_count = 0, 0
+        with torch.no_grad():
+            for x, y, _ in val_loader:
+                x = x.to(device, non_blocking=True)
+                y = y.to(device, non_blocking=True)
+                _, logits = model(x)
+                val_correct += (logits.argmax(1) == y).sum().item()
+                val_count   += x.size(0)
+
+        tr_acc = train_correct / max(1, train_count)
+        va_acc = val_correct   / max(1, val_count)
+        history["train_acc"].append(tr_acc)
+        history["val_acc"].append(va_acc)
+
+        print(f"Epoch {epoch}:: train accuracy: {tr_acc:.3f} | val accuracy: {va_acc:.3f}")
 
 
 if __name__ == "__main__":
